e3899478a3f6ec92be10b93a7876b89d4fb406ef
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-11-17 05:53:43 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,20 @@
 document.querySelector('button').onclick = () => {
-  let img = navigator.clipboard.readText()
-
-  const Http = new XMLHttpRequest();
-  const url = 'https://getImagePixels.wonk2.repl.co';
-  Http.open("GET", url);
-  Http.send();
+  navigator.clipboard.readText().then(text => {
+    const Http = new XMLHttpRequest();
+    Http.open("POST", 'https://getImagePixels.wonk2.repl.co', true);
+    Http.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
   
-  Http.onreadystatechange = (e) => {
-    console.log(Http.responseText)
-  }
+    let params = {
+      "imageURL": text,
+      "width": 50,
+      "height": 50,
+      "useCustomSizeProps": false
+    }
+    
+    Http.send(JSON.stringify(params));
+    
+    Http.onreadystatechange = (e) => {
+      navigator.clipboard.writeText(Http.responseText)
+    }
+  })
 }
