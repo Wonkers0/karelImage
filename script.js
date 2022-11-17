fee251c881fc376a1395805ec6e18bcb0a935944
Wonk <106038003+Wonkers0@users.noreply.github.com>
2022-11-17 07:09:10 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,24 @@
+const checkInput = (input) => {
+  if(!parseInt(input.value)) input.value = 50 // input not a number
+  if (parseInt(input.value) < 1) input.value = 1 // Input less than 1
+  if (parseInt(input.value) > 50) input.value = 50 // Input bigger than 50
+}
+
+document.querySelector('.github').onclick = () => {
+  window.location.href = 'https://github.com/Wonkers0/karelImage'
+}
+
 document.querySelector('button').onclick = () => {
   navigator.clipboard.readText().then(text => {
     const Http = new XMLHttpRequest();
     Http.open("POST", 'https://getImagePixels.wonk2.repl.co', true);
     Http.setRequestHeader('Content-Type', 'application/json;charset=UTF-8')
-  
+    
     let params = {
       "imageURL": text,
-      "width": 50,
-      "height": 50,
-      "useCustomSizeProps": false
+      "width": !parseInt(document.querySelector('#width').value) ? 50 : parseInt(document.querySelector('#width').value),
+      "height": !parseInt(document.querySelector('#height').value) ? 50 : parseInt(document.querySelector('#height').value),
+      "useCustomSizeProps": !document.querySelector('.useCustomProps').checked
     }
     
     Http.send(JSON.stringify(params));
